47cbfd7592b04fc77d5c34bef0ef0e2c670329f9
fridizkool <fsorgtaylor@gmail.com>
2024-05-15 16:26:10 -0400

    Page history initial page

--- src/pages/Transactions/TransactionHistory.tsx
+++ src/pages/Transactions/TransactionHistory.tsx
@@ -1,9 +1,46 @@
+import { Card, CardBody, CardGroup, CardHeader, Table, Title } from "@trussworks/react-uswds";
+
 const TransactionHistory: React.FC = () => {
+    const Name: string = "temp";
+    const total: number = 1000.00;
+    const transactions: number = 2;
     return (
-        <section>
-            <h1>Transaction History</h1>
-            <p>Welcome to the Transaction History!</p>
-        </section>
+        <>
+            <div className="px-5">
+                <Title>{`${Name} transaction history`}</Title>
+                <CardGroup>
+                    <Card gridLayout={{ col: 8 }}>
+                        <CardHeader>
+                            <h1>All transactions</h1>
+                        </CardHeader>
+                        <CardBody>
+                            <Table bordered={false} fullWidth={true}>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>Name</th>
+                                    <th>Category</th>
+                                    <th>Actions</th>
+                                    <th>Amount</th>
+                                </tr>
+                                <tr>
+                                    <td>
+
+                                    </td>
+                                </tr>
+                            </Table>
+                        </CardBody>
+                    </Card>
+                    <Card gridLayout={{ col: 4 }}>
+                        <CardHeader>Summary</CardHeader>
+                        <CardBody>
+                            Total spent: {total}
+                            <hr />
+                            Total transactions: {transactions}
+                        </CardBody>
+                    </Card>
+                </CardGroup>
+            </div>
+        </>
     )
 }
 
