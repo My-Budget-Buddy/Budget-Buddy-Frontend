8cb514564eacfe5c4b88e39225cea052632cc06e
Kyle Huynh <kyle_huynh@hotmail.com>
2024-05-16 07:46:50 -0700

    component contents centered

--- src/pages/Spending/Spending.tsx
+++ src/pages/Spending/Spending.tsx
@@ -9,18 +9,22 @@ const Spending: React.FC = () => {
       </div>
       <div className="flex-1 ml-64">
         <section className="overflow-auto h-screen">
-          <Title className="ml-3">Spending Overview</Title> {/* Title for the page */}
+          <Title className="ml-3">Spending Overview</Title>{" "}
+          {/* Title for the page */}
           {/* Full-width row */}
-          <div className="bg-blue-300 p-4 m-2 min-h-[30rem] rounded-md">
+          <div className="bg-blue-300 p-4 m-2 min-h-[30rem] rounded-md flex justify-center items-center">
             <h2>Full Width Component</h2>
+            {/* More content here */}
           </div>
           {/* Second row with two columns */}
           <div className="flex">
-            <div className="flex-3 bg-green-300 p-4 m-2 min-h-[30rem] rounded-md">
+            <div className="flex justify-center items-center flex-3 bg-green-300 p-4 m-2 min-h-[30rem] rounded-md ">
               <h2>Large Pie Chart</h2>
+              {/* More content here */}
             </div>
-            <div className="flex-1 bg-yellow-300 p-4 m-2 rounded-md">
+            <div className="flex justify-center items-center flex-1 bg-yellow-300 p-4 m-2 rounded-md">
               <h2>Text or Info</h2>
+              {/* More content here */}
             </div>
           </div>
         </section>
