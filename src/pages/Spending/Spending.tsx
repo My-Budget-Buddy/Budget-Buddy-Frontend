b90adcd5b593dc448b5b56531029970d43dce04c
Kyle Huynh <kyle_huynh@hotmail.com>
2024-05-16 08:39:33 -0700

    pie chart

--- src/pages/Spending/Spending.tsx
+++ src/pages/Spending/Spending.tsx
@@ -1,10 +1,21 @@
 import { Title } from "@trussworks/react-uswds";
+import { PieChart } from "@mui/x-charts";
+
+const data = [
+  {
+    data: [
+      { id: 0, value: 10, label: "series A" },
+      { id: 1, value: 15, label: "series B" },
+      { id: 2, value: 20, label: "series C" },
+    ],
+  },
+];
 
 const Spending: React.FC = () => {
   return (
-    <div className="flex">
+    <div className="min-w-screen">
       <div className="flex-1">
-        <section className="overflow-auto h-screen">
+        <section className=" h-screen">
           <Title className="ml-3">Spending Overview</Title>{" "}
           {/* Title for the page */}
           {/* Full-width row */}
@@ -15,8 +26,7 @@ const Spending: React.FC = () => {
           {/* Second row with two columns */}
           <div className="flex">
             <div className="flex justify-center items-center flex-3 bg-green-300 p-4 m-2 min-h-[30rem] rounded-md ">
-              <h2>Large Pie Chart</h2>
-              {/* More content here */}
+              <PieChart series={data} width={600} height={300} />
             </div>
             <div className="flex justify-center items-center flex-1 bg-yellow-300 p-4 m-2 rounded-md">
               <h2>Text or Info</h2>
