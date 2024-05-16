e745637993bc50b9d5aa3b6a037252e5ab12d1bb
Clemente Solorio <solorioeclem164@gmail.com>
2024-05-16 10:21:25 -0700

    account modal prints the form field submission

--- src/pages/Accounts/AccountModal.tsx
+++ src/pages/Accounts/AccountModal.tsx
@@ -1,79 +1,138 @@
-import { ButtonGroup, Label, Modal, ModalFooter, ModalHeading, ModalRef, ModalToggleButton, Select, TextInput, TextInputMask } from "@trussworks/react-uswds";
-import React from "react";
+import {
+  Label,
+  Modal,
+  Select,
+  ModalRef,
+  TextInput,
+  ButtonGroup,
+  ModalFooter,
+  ModalHeading,
+  TextInputMask,
+  ModalToggleButton,
+  Form,
+  Button,
+} from "@trussworks/react-uswds";
+import React, { FormEvent } from "react";
 import { useRef } from "react";
 
 const AccountModal: React.FC = () => {
-    const modalRef = useRef<ModalRef>(null);
-    return (
-        <>
-            <div>
-                <ModalToggleButton modalRef={modalRef} opener>
-                    Add Account
+  const modalRef = useRef<ModalRef>(null);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // TODO: update the keys to match the schema that the backend expects
+    const fields = {
+      //@ts-expect-error elements aren't typed
+      type: e.currentTarget.elements["account-type"].value,
+      //@ts-expect-error elements aren't typed
+      name: e.currentTarget.elements["input-account-name"].value,
+      //@ts-expect-error elements aren't typed
+      accountNumber: e.currentTarget.elements["input-account-num"].value,
+      //@ts-expect-error elements aren't typed
+      balance: e.currentTarget.elements["account-balance"].value,
+    };
+
+    console.log(fields);
+
+    // close the modal
+    modalRef.current?.toggleModal();
+    return;
+  };
+
+  return (
+    <>
+      <div>
+        <ModalToggleButton modalRef={modalRef} opener>
+          Add Account
+        </ModalToggleButton>
+        <Modal
+          ref={modalRef}
+          id="account-modal"
+          aria-labelledby="account-modal-heading"
+          aria-describedby="account-modal-description"
+        >
+          <ModalHeading id="account-modal-heading">
+            Enter the Account Details
+          </ModalHeading>
+          <Form onSubmit={handleSubmit} className="usa-prose">
+            <Label
+              id="label-account-type"
+              htmlFor="account-type"
+              requiredMarker
+            >
+              Account Type
+            </Label>
+            <Select
+              id="account-type"
+              name="account-type"
+              aria-labelledby="label-account-type"
+              required
+            >
+              <React.Fragment key=".0">
+                <option>- Select - </option>
+                <option value="checking">Checking</option>
+                <option value="savings">Savings</option>
+                <option value="Credit">Credit</option>
+                <option value="investments">Investments</option>
+              </React.Fragment>
+            </Select>
+
+            <Label htmlFor="input-account-name" requiredMarker>
+              Account Name
+            </Label>
+            <TextInput
+              id="input-account-name"
+              name="input-account-name"
+              type="text"
+              required
+            />
+
+            <Label id="label-account-num" htmlFor="account-num" requiredMarker>
+              Account Number
+            </Label>
+            <span id="hint-num" className="usa-hint">
+              For example, 0000
+            </span>
+            <TextInputMask
+              id="input-account-num"
+              name="input-account-num"
+              type="text"
+              aria-labelledby="label-account-num"
+              aria-describedby="hint-num"
+              mask="____"
+              pattern="\d{4}"
+              required
+            />
+
+            <Label htmlFor="account-balance" requiredMarker>
+              Balance
+            </Label>
+            <TextInput
+              id="account-balance"
+              name="account-balance"
+              type="text"
+              required
+            />
+
+            <ModalFooter>
+              <ButtonGroup>
+                <Button type="submit">Add Account</Button>
+                <ModalToggleButton
+                  modalRef={modalRef}
+                  closer
+                  unstyled
+                  className="padding-105 text-center"
+                >
+                  Go back
                 </ModalToggleButton>
-                <Modal ref={modalRef} id="account-modal" aria-labelledby="account-modal-heading" aria-describedby="account-modal-description">
-                    <ModalHeading id="account-modal-heading">
-                        Enter the Account Details
-                    </ModalHeading>
-                    <div className="usa-prose">
-                        <Label id="account-type" htmlFor="account-type">
-                            Account Type
-                        </Label>
-                        <Select
-                            id="account-select"
-                            name="account-select"
-                        >
-                            <React.Fragment key=".0">
-                                <option>
-                                    - Select -{' '}
-                                </option>
-                                <option value="checking">
-                                    Checking
-                                </option>
-                                <option value="savings">
-                                    Savings
-                                </option>
-                                <option value="Credit">
-                                    Credit
-                                </option>
-                                <option value="investments">
-                                    Investments
-                                </option>
-                            </React.Fragment>
-                        </Select>
+              </ButtonGroup>
+            </ModalFooter>
+          </Form>
+        </Modal>
+      </div>
+    </>
+  );
+};
 
-                        <Label id="account-name" htmlFor="account-name">
-                            Account Name
-                        </Label>
-                        <TextInput id="input-account-name" name="input-account-name" type="text" />
-
-                        <Label id="account-num" htmlFor="account-num">
-                            Account Number
-                        </Label>
-                        <span id="hint-num" className="usa-hint">
-                            For example, 0000
-                        </span>
-                        <TextInputMask id="input-type-account-num" name="input-type-account-num" type="text" aria-labelledby="account-num" aria-describedby="hint-num" mask="____" pattern="\d{4}" />
-
-                        <Label id="account-balance" htmlFor="account-balance">
-                            Balance
-                        </Label>
-                        <TextInput id="input-balance" name="input-balance" type="text" />
-
-                    </div>
-                    <ModalFooter>
-                        <ButtonGroup>
-                            <ModalToggleButton modalRef={modalRef} closer>
-                                Add Account
-                            </ModalToggleButton>
-                            <ModalToggleButton modalRef={modalRef} closer unstyled className="padding-105 text-center">
-                                Go back
-                            </ModalToggleButton>
-                        </ButtonGroup>
-                    </ModalFooter>
-                </Modal>
-            </div>
-        </>
-    )
-}
-
-export default AccountModal
+export default AccountModal;
--- src/pages/Accounts/Accounts.tsx
+++ src/pages/Accounts/Accounts.tsx
@@ -1,150 +1,240 @@
-import { Accordion, Grid, GridContainer, Icon } from "@trussworks/react-uswds"
+import { Accordion, Grid, GridContainer, Icon } from "@trussworks/react-uswds";
 
 import AccountModal from "./AccountModal";
 
 const Accounts: React.FC = () => {
+  return (
+    <>
+      <h1>Accounts</h1>
+      <div className="flex justify-end">
+        <AccountModal />
+      </div>
 
-    return (
-        <>
-            <h1>Accounts</h1>
-            <div className="px-10 py-2 flex justify-end">
-                <AccountModal  />
-            </div>
-            
-            <div className="px-10 py-5">
-                <Accordion bordered={false} multiselectable={true} items={
-                    [{
-                        title: (
-                            <div className="flex justify-between">
-                                <p><Icon.AccountBalance /> Checking</p>
-                            </div>
-                        ),
-                        content: (
-                            <div>
-                                <GridContainer>
-                                    <Grid row>
-                                        <Grid tablet={{
-                                            col: 2
-                                        }}>{<p>TD Bank</p>}</Grid>
-                                        <Grid className="flex justify-start" tablet={{
-                                            col: 4 
-                                        }}>{<p>0001</p>}</Grid>
-                                        <Grid className="flex justify-end" tablet={{
-                                            col: 6
-                                        }}>{<p>$5,000</p>}</Grid>
-                                    </Grid>
-                                </GridContainer>
-                            </div>
-                        ),
-                        expanded: false,
-                        id: "Checking",
-                        headingLevel: "h4",
-                    }
-                        ,
-                    {
-                        title: (
-                            <div className="flex justify-between">
-                                <p><Icon.CreditCard /> Credit Cards</p>
-                            </div>
-                        ),
-                        content: (<div>
-                            <GridContainer>
-                                <Grid row>
-                                    <Grid tablet={{
-                                        col: 2
-                                    }}>{<p>Visa</p>}</Grid>
-                                    <Grid className="flex justify-start" tablet={{
-                                        col: 4 
-                                    }}>{<p>5000</p>}</Grid>
-                                    <Grid className="flex justify-end" tablet={{
-                                        col: 6
-                                    }}>{<p>$2,310</p>}</Grid>
-                                </Grid>
-                            </GridContainer>
-                        </div>),
-                        expanded: false,
-                        id: "credit-cards",
-                        headingLevel: "h4",
-                    },
-                    {
-                        title: (
-                            <div className="flex justify-between">
-                                <p><Icon.AccountBalance /> Net Cash</p>
-                            </div>
-                        ),
-                        content: (<div>
-                            <GridContainer>
-                                <Grid row>
-                                    <Grid tablet={{
-                                        col: 2
-                                    }}>{<p>TD Bank</p>}</Grid>
-                                    <Grid className="flex justify-start" tablet={{
-                                        col: 4 
-                                    }}>{<p>0001</p>}</Grid>
-                                    <Grid className="flex justify-end" tablet={{
-                                        col: 6
-                                    }}>{<p>$5,000</p>}</Grid>
-                                </Grid>
-                            </GridContainer>
-                        </div>),
-                        expanded: false,
-                        id: "net-cash",
-                        headingLevel: "h4",
-                    },
-                    {
-                        title: (
-                            <div className="flex justify-between">
-                                <p><Icon.AccountBalance /> Savings</p>
-                            </div>
-                        ),
-                        content: (<div>
-                            <GridContainer>
-                                <Grid row>
-                                    <Grid tablet={{
-                                        col: 2
-                                    }}>{<p>TD Bank</p>}</Grid>
-                                    <Grid className="flex justify-start" tablet={{
-                                        col: 4 
-                                    }}>{<p>0009</p>}</Grid>
-                                    <Grid className="flex justify-end" tablet={{
-                                        col: 6
-                                    }}>{<p>$10,000</p>}</Grid>
-                                </Grid>
-                            </GridContainer>
-                        </div>),
-                        expanded: false,
-                        id: "savings",
-                        headingLevel: "h4",
-                    },
-                    {
-                        title: (
-                            <div className="flex justify-between">
-                                <p><Icon.AccountBalance /> investments</p>
-                            </div>
-                        ),
-                        content: (<div>
-                            <GridContainer>
-                                <Grid row>
-                                    <Grid tablet={{
-                                        col: 2
-                                    }}>{<p>Stocks</p>}</Grid>
-                                    <Grid className="flex justify-start" tablet={{
-                                        col: 4 
-                                    }}>{<p>0060</p>}</Grid>
-                                    <Grid className="flex justify-end" tablet={{
-                                        col: 6
-                                    }}>{<p>$2,000</p>}</Grid>
-                                </Grid>
-                            </GridContainer>
-                        </div>),
-                        expanded: false,
-                        id: "investments",
-                        headingLevel: "h4",
-                    }]
-                } />
-            </div>
-        </>
-    )
-}
+      <div className="py-5">
+        <Accordion
+          bordered={false}
+          multiselectable={true}
+          items={[
+            {
+              title: (
+                <div className="flex justify-between">
+                  <p>
+                    <Icon.AccountBalance /> Checking
+                  </p>
+                </div>
+              ),
+              content: (
+                <div>
+                  <GridContainer>
+                    <Grid row>
+                      <Grid
+                        tablet={{
+                          col: 2,
+                        }}
+                      >
+                        {<p>TD Bank</p>}
+                      </Grid>
+                      <Grid
+                        className="flex justify-start"
+                        tablet={{
+                          col: 4,
+                        }}
+                      >
+                        {<p>0001</p>}
+                      </Grid>
+                      <Grid
+                        className="flex justify-end"
+                        tablet={{
+                          col: 6,
+                        }}
+                      >
+                        {<p>$5,000</p>}
+                      </Grid>
+                    </Grid>
+                  </GridContainer>
+                </div>
+              ),
+              expanded: false,
+              id: "Checking",
+              headingLevel: "h4",
+            },
+            {
+              title: (
+                <div className="flex justify-between">
+                  <p>
+                    <Icon.CreditCard /> Credit Cards
+                  </p>
+                </div>
+              ),
+              content: (
+                <div>
+                  <GridContainer>
+                    <Grid row>
+                      <Grid
+                        tablet={{
+                          col: 2,
+                        }}
+                      >
+                        {<p>Visa</p>}
+                      </Grid>
+                      <Grid
+                        className="flex justify-start"
+                        tablet={{
+                          col: 4,
+                        }}
+                      >
+                        {<p>5000</p>}
+                      </Grid>
+                      <Grid
+                        className="flex justify-end"
+                        tablet={{
+                          col: 6,
+                        }}
+                      >
+                        {<p>$2,310</p>}
+                      </Grid>
+                    </Grid>
+                  </GridContainer>
+                </div>
+              ),
+              expanded: false,
+              id: "credit-cards",
+              headingLevel: "h4",
+            },
+            {
+              title: (
+                <div className="flex justify-between">
+                  <p>
+                    <Icon.AccountBalance /> Net Cash
+                  </p>
+                </div>
+              ),
+              content: (
+                <div>
+                  <GridContainer>
+                    <Grid row>
+                      <Grid
+                        tablet={{
+                          col: 2,
+                        }}
+                      >
+                        {<p>TD Bank</p>}
+                      </Grid>
+                      <Grid
+                        className="flex justify-start"
+                        tablet={{
+                          col: 4,
+                        }}
+                      >
+                        {<p>0001</p>}
+                      </Grid>
+                      <Grid
+                        className="flex justify-end"
+                        tablet={{
+                          col: 6,
+                        }}
+                      >
+                        {<p>$5,000</p>}
+                      </Grid>
+                    </Grid>
+                  </GridContainer>
+                </div>
+              ),
+              expanded: false,
+              id: "net-cash",
+              headingLevel: "h4",
+            },
+            {
+              title: (
+                <div className="flex justify-between">
+                  <p>
+                    <Icon.AccountBalance /> Savings
+                  </p>
+                </div>
+              ),
+              content: (
+                <div>
+                  <GridContainer>
+                    <Grid row>
+                      <Grid
+                        tablet={{
+                          col: 2,
+                        }}
+                      >
+                        {<p>TD Bank</p>}
+                      </Grid>
+                      <Grid
+                        className="flex justify-start"
+                        tablet={{
+                          col: 4,
+                        }}
+                      >
+                        {<p>0009</p>}
+                      </Grid>
+                      <Grid
+                        className="flex justify-end"
+                        tablet={{
+                          col: 6,
+                        }}
+                      >
+                        {<p>$10,000</p>}
+                      </Grid>
+                    </Grid>
+                  </GridContainer>
+                </div>
+              ),
+              expanded: false,
+              id: "savings",
+              headingLevel: "h4",
+            },
+            {
+              title: (
+                <div className="flex justify-between">
+                  <p>
+                    <Icon.AccountBalance /> investments
+                  </p>
+                </div>
+              ),
+              content: (
+                <div>
+                  <GridContainer>
+                    <Grid row>
+                      <Grid
+                        tablet={{
+                          col: 2,
+                        }}
+                      >
+                        {<p>Stocks</p>}
+                      </Grid>
+                      <Grid
+                        className="flex justify-start"
+                        tablet={{
+                          col: 4,
+                        }}
+                      >
+                        {<p>0060</p>}
+                      </Grid>
+                      <Grid
+                        className="flex justify-end"
+                        tablet={{
+                          col: 6,
+                        }}
+                      >
+                        {<p>$2,000</p>}
+                      </Grid>
+                    </Grid>
+                  </GridContainer>
+                </div>
+              ),
+              expanded: false,
+              id: "investments",
+              headingLevel: "h4",
+            },
+          ]}
+        />
+      </div>
+    </>
+  );
+};
 
-export default Accounts
+export default Accounts;
