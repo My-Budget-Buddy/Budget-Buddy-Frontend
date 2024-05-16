28f466e4ce7d8f72ec6283693bffe56fa964a49d
Andrea <andrearewards@hotmail.com>
2024-05-16 10:18:08 -0400

    create outline for navbar with links to all pages

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,13 +1,13 @@
 import {Outlet} from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 const AppLayout: React.FC = () => {
     return(
-        <div className='layout-container'>
-            {/*<SideNavBar />*/}
+        <div className='layout-container flex'>
+            <NavBar />
             <main className='main-content'>
                 <Outlet />
             </main>
-
         </div>
     )
 }
