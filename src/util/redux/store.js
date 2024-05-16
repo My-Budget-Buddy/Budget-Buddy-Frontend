365ee59e9428247e1161de7e2d4ff6876275e497
Kaiwen Tsou <kktsou@ucsd.edu>
2024-05-16 13:22:09 -0700

    Fixed wrong name

--- src/util/redux/store.js
+++ src/util/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import budgetReducer from './counterSlice'
+import budgetReducer from './budgetSlice'
 
 export default configureStore({
   reducer: {
