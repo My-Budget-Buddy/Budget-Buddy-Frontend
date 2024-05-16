25f89af4d2585759fa9f0d472bae74586aa7234b
Clemente Solorio <solorioeclem164@gmail.com>
2024-05-16 07:38:27 -0700

    minor changes to nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,51 +1,61 @@
-import { Button, Icon } from "@trussworks/react-uswds";
 import { Link } from "react-router-dom";
+import { Icon } from "@trussworks/react-uswds";
 
+const pages = [
+  {
+    path: "/dashboard",
+    title: "Dashboard",
+    icon: Icon.List,
+  },
+  {
+    path: "/dashboard/accounts",
+    title: "Accounts",
+    icon: Icon.AccountBalance,
+  },
+  {
+    path: "/dashboard/budgets",
+    title: "Budgets",
+    icon: Icon.Assessment,
+  },
+  {
+    path: "/dashboard/spending",
+    title: "Spending",
+    icon: Icon.Assessment,
+  },
+  {
+    path: "/dashboard/transactions",
+    title: "Transactions",
+    icon: Icon.Search,
+  },
+  {
+    path: "/dashboard/tax",
+    title: "Tax",
+    icon: Icon.Assessment,
+  },
+];
 
 const NavBar = () => {
-    return ( 
-        <div className="nav-container bg-base-lighter">
-            <div className="flex flex-col items-center ">
-                <h1>Budget Buddy</h1>
-                <div className="nav-settings flex flex-row items-center">
-                    <h3>Hi, [Name]</h3>
-                    <Icon.Settings/>
-                </div>
-            </div>
-            <div className="nav-btns flex flex-col items-center">
-                <Link to="/dashboard">
-                    <Button type="submit" >
-                        <Icon.List/> Dashboard
-                    </Button>
-                </Link>
-                <Link to="/dashboard/accounts">
-                    <Button type="submit" >
-                        <Icon.AccountBalance/> Accounts
-                    </Button>
-                </Link>
-                <Link to="/dashboard/budgets">
-                    <Button type="submit" >
-                        <Icon.Assessment/> Budgets
-                    </Button>
-                </Link>
-                <Link to="/dashboard/spending">
-                    <Button type="submit" >
-                        <Icon.Assessment/>Spending
-                    </Button>
-                </Link>
-                <Link to="/dashboard/transactions">
-                    <Button type="submit" >
-                        <Icon.Search/>Transactions
-                    </Button>
-                </Link>
-                <Link to="/dashboard/tax">
-                    <Button type="submit" >
-                        <Icon.Assessment/>Tax
-                    </Button>
-                </Link>
-            </div>
+  return (
+    <div className="nav-container bg-base-lighter px-6">
+      <div className="flex flex-col items-center">
+        <h1 className="font-semibold">Budget Buddy</h1>
+        <div className="flex flex-row items-center gap-4">
+          <h3>Hi, [Name]</h3>
+          <Icon.Settings />
         </div>
-    );
-}
- 
+      </div>
+      <div className="flex flex-col items-center gap-6 mt-14">
+        {pages.map((pages) => (
+          <Link
+            to={pages.path}
+            className="w-full usa-button min-w-full text-left items-center"
+          >
+            <pages.icon className="mr-3" /> {pages.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default NavBar;
