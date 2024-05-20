c71d53a6614211799abd5ac3d45ee35c144c93ab
Kaiwen Tsou <kktsou@ucsd.edu>
2024-05-20 06:14:10 -0700

    Fixed merge conflict

--- src/util/redux/store.ts
+++ src/util/redux/store.ts
@@ -1,14 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import budgetReducer from './budgetSlice'
 import formSubmissionReducer from './formSubmissionStateSlice'
+import w2Reducer from "../../pages/Tax/W2Slice";
 
 export const store = configureStore({
   reducer: {
     budgets: budgetReducer,
-    formStatus: formSubmissionReducer
+    formStatus: formSubmissionReducer,
+    w2 : w2Reducer
   }
 })
 
+export default store;
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
