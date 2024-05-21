ab0169a759b6e98a8bb711216a410d13f1f5b89c
Koudou Sugawara <ksugawara01@gmail.com>
2024-05-21 13:38:36 -0400

    moved selected month and year to the redux store

--- src/pages/Budgets/components/BudgetsComponent.tsx
+++ src/pages/Budgets/components/BudgetsComponent.tsx
@@ -1,14 +1,17 @@
-import { Table, Button, ButtonGroup, Icon } from '@trussworks/react-uswds'
-import BudgetsRow from './subComponents/BudgetsRow'
-import NewBudgetModal from './modals/NewBudgetModal'
-import { useEffect, useState } from 'react';
+import { Table, Button, ButtonGroup, Icon } from "@trussworks/react-uswds";
+import BudgetsRow from "./subComponents/BudgetsRow";
+import NewBudgetModal from "./modals/NewBudgetModal";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateSelectedDate } from "../../../util/redux/budgetSlice";
 
 const BudgetsComponent: React.FC = () => {
-
     const [budgets, setBudgets] = useState([]);
 
+    const budgetsStore = useSelector((store: any) => store.budgets);
+    const dispatch = useDispatch();
+
     const currentDate = new Date();
-    const months = ["Jan.","Feb.","Mar.","Apr.","May","Jun.","Jul.","Aug.","Sep.","Oct.","Nov.","Dec."];
 
     // the month and year that the user has selected
     const [selectedDate, setSelectedDate] = useState<Date>(new Date(currentDate.setDate(1)));
@@ -21,44 +24,59 @@ const BudgetsComponent: React.FC = () => {
     let nextMonthDate = new Date(selectedDate);
     nextMonthDate.setMonth(nextMonthDate.getMonth() + 1);
 
-    // get selected month as a string
-    const selectedMonth = months[selectedDate.getMonth()];
-    const selectedYear = selectedDate.getFullYear();
-    
     useEffect(() => {
         /* GET budgets
             .then(response => {
                 setBudgets(response)
             })
         */
-    }, [])
+    }, []);
 
     const selectPreviousMonth = () => {
+        const selectedMonthYear = {
+            selectedMonth: previousMonthDate.getMonth(),
+            selectedYear: previousMonthDate.getFullYear()
+        };
+        dispatch(updateSelectedDate(selectedMonthYear));
         setSelectedDate(new Date(previousMonthDate));
-    }
+    };
 
     const selectNextMonth = () => {
+        const selectedMonthYear = {
+            selectedMonth: nextMonthDate.getMonth(),
+            selectedYear: nextMonthDate.getFullYear()
+        };
+        dispatch(updateSelectedDate(selectedMonthYear));
         setSelectedDate(new Date(nextMonthDate));
-    }
+    };
 
     return (
         <>
-            {/* May 2024 Budget ------  PreviousMonthButton -- NextMonthButton */}
-            <div className='flex w-full'>
-                <h1 className='font-bold mr-4'>{selectedMonth} {selectedYear} Budget</h1>
+            <div className="flex w-full">
+                <h1 className="font-bold mr-4">
+                    {budgetsStore.selectedMonthString} {budgetsStore.selectedYear} Budget
+                </h1>
                 <ButtonGroup>
-                    <Button type='button' onClick={selectPreviousMonth}><Icon.NavigateBefore /> {months[previousMonthDate.getMonth()]} {previousMonthDate.getFullYear()}</Button>
+                    <Button type="button" onClick={selectPreviousMonth}>
+                        <Icon.NavigateBefore /> {budgetsStore.months[previousMonthDate.getMonth()]}{" "}
+                        {previousMonthDate.getFullYear()}
+                    </Button>
                     {/* disable the next month button if it is more than one month past the current month */}
-                    {   
-                        selectedDate > currentDate ?
-                        <Button type='button' onClick={selectNextMonth} disabled>{months[nextMonthDate.getMonth()]} {nextMonthDate.getFullYear()} <Icon.NavigateNext/></Button>
-                        : <Button type='button' onClick={selectNextMonth}>{months[nextMonthDate.getMonth()]} {nextMonthDate.getFullYear()} <Icon.NavigateNext/></Button>
-                    }
+                    {selectedDate > currentDate ? (
+                        <Button type="button" onClick={selectNextMonth} disabled>
+                            {budgetsStore.months[nextMonthDate.getMonth()]} {nextMonthDate.getFullYear()}{" "}
+                            <Icon.NavigateNext />
+                        </Button>
+                    ) : (
+                        <Button type="button" onClick={selectNextMonth}>
+                            {budgetsStore.months[nextMonthDate.getMonth()]} {nextMonthDate.getFullYear()}{" "}
+                            <Icon.NavigateNext />
+                        </Button>
+                    )}
                 </ButtonGroup>
             </div>
 
-        
-            <Table className='w-full'>
+            <Table className="w-full">
                 <thead>
                     <tr>
                         <th>Budget Category</th>
@@ -77,17 +95,22 @@ const BudgetsComponent: React.FC = () => {
                         calculate actual of current row from transactions
                         <BudgetsRow />
                     }) */}
-                    <BudgetsRow category='Phone Bill' budgeted={55} actual={0} isReserved={true} notes='' />
-                    <BudgetsRow category='Gasoline' budgeted={80} actual={95} isReserved={false} notes='Road trip coming up!' />
-                    <BudgetsRow category='Restaurants' budgeted={155} actual={88} isReserved={false} notes='' />
+                    <BudgetsRow category="Phone Bill" budgeted={55} actual={0} isReserved={true} notes="" />
+                    <BudgetsRow
+                        category="Gasoline"
+                        budgeted={80}
+                        actual={95}
+                        isReserved={false}
+                        notes="Road trip coming up!"
+                    />
+                    <BudgetsRow category="Restaurants" budgeted={155} actual={88} isReserved={false} notes="" />
                 </tbody>
             </Table>
-            <div className='flex flex-col items-center'>
+            <div className="flex flex-col items-center">
                 <NewBudgetModal />
             </div>
-            
         </>
-    )
-}
+    );
+};
 
-export default BudgetsComponent
+export default BudgetsComponent;
