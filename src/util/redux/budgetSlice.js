45a686ba06460e88bc081601d598f1d8925b3b3d
Koudou Sugawara <ksugawara01@gmail.com>
2024-05-21 09:11:45 -0400

    add redux slices

--- src/util/redux/budgetSlice.js
+++ src/util/redux/budgetSlice.js
@@ -1,23 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit"
 
 export const budgetSlice = createSlice({
-  name: 'budgetSummary',
+  name: "budgets",
   initialState: {
-    totalAvailableFunds: 0,
-    totalSpendingBudget: 0,
+    budgets: [],
+    totalFundsAvailable: 0, 
+    spendingBudget: 0, 
+    totalReserved: 0,
     totalActuallySpent: 0
   },
   reducers: {
-    //update total available funds
-    //update spending budget (what you CAN spend)
-    //update total actually spent
-    
-    update: state => {
-        state.value += 1; 
+
+    updateSpendingBudget(state, action) {
+      const spendingBudget = action.payload;
+      state.spendingBudget = action.payload;
+    },
+
+    updateBudgets(state, action) {
+      const budgets = action.payload;
+      state.budgets = budgets;
+    },
+  
+    // pass in the amount to be reserved (or a negative amount for unreserving)
+    updateTotalReserved(state, action) {
+      const reservedAmount = action.payload;
+      state.totalReserved += reservedAmount; 
+    },
+
+    updateTotalActuallySpent(state, action) {
+      const totalSpent = action.payload;
+      state.totalActuallySpent += totalSpent;
     }
   }
 })
 
-export const { update } = budgetSlice.actions
+export const { updateSpendingBudget, updateBudgets, updateTotalReserved, updateTotalActuallySpent } = budgetSlice.actions
 
 export default budgetSlice.reducer
