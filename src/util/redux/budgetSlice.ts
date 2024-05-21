4d73c13e55987ca5c610a9e362fde86fbe4e4a19
Kaiwen Tsou <kktsou@ucsd.edu>
2024-05-21 12:21:06 -0700

    Fixed some merge conflicts

--- src/util/redux/budgetSlice.ts
+++ src/util/redux/budgetSlice.ts
@@ -1,5 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// default date values
+const currentDate = new Date();
+const months = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
+const selectedMonth = currentDate.getMonth();
+const selectedMonthString = months[currentDate.getMonth()];
+const selectedYear = currentDate.getFullYear();
+
 export const budgetSlice = createSlice({
     name: "budgets",
     initialState: {
@@ -7,7 +14,11 @@ export const budgetSlice = createSlice({
         totalFundsAvailable: 0,
         spendingBudget: 0,
         totalReserved: 0,
-        totalActuallySpent: 0
+        totalActuallySpent: 0,
+        months: months,
+        selectedMonth: selectedMonth,
+        selectedMonthString: selectedMonthString,
+        selectedYear: selectedYear
     },
     reducers: {
         updateSpendingBudget(state, action) {
@@ -29,11 +40,28 @@ export const budgetSlice = createSlice({
         updateTotalActuallySpent(state, action) {
             const totalSpent = action.payload;
             state.totalActuallySpent += totalSpent;
+        },
+
+        // pass in the selectedDate date object
+        updateSelectedDate(state, action) {
+            const selectedDate = action.payload;
+            const selectedMonth = selectedDate.selectedMonth;
+            const selectedMonthString = months[selectedMonth];
+            const selectedYear = selectedDate.selectedYear;
+
+            state.selectedMonth = selectedMonth;
+            state.selectedMonthString = selectedMonthString;
+            state.selectedYear = selectedYear;
         }
     }
 });
 
-export const { updateSpendingBudget, updateBudgets, updateTotalReserved, updateTotalActuallySpent } =
-    budgetSlice.actions;
+export const {
+    updateSpendingBudget,
+    updateBudgets,
+    updateTotalReserved,
+    updateTotalActuallySpent,
+    updateSelectedDate
+} = budgetSlice.actions;
 
 export default budgetSlice.reducer;
