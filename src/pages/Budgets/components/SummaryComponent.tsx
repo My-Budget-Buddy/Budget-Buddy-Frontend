cba547e65a94bedf2c873500a77bd354a5c47f40
Koudou Sugawara <ksugawara01@gmail.com>
2024-05-21 09:12:47 -0400

    use data from redux store

--- src/pages/Budgets/components/SummaryComponent.tsx
+++ src/pages/Budgets/components/SummaryComponent.tsx
@@ -1,22 +1,41 @@
-import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
+import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const SummaryComponent: React.FC = () => {
+
+    const budgetsStore = useSelector((store : any) => store.budgets)
+    const bucketsStore = useSelector((store : any) => store.buckets)
+
+    const currentDate = new Date();
+    const months = ["Jan.","Feb.","Mar.","Apr.","May","Jun.","Jul.","Aug.","Sep.","Oct.","Nov.","Dec."];
+
+    // get selected month as a string
+    const currentMonth = months[currentDate.getMonth()];
+    const currentYear = currentDate.getFullYear();
+
+    const remainingBudget = (budgetsStore.spendingBudget - budgetsStore.totalReserved - bucketsStore.totalReserved - budgetsStore.totalActuallySpent).toString();
+    const percentageRemaining = Number(remainingBudget)/budgetsStore.spendingBudget*100;
+
+    useEffect(() => {
+        /**/
+    }, [/*budgets, buckets*/])
     return (
         <>
-            <div className='flex flex-row justify-between w-full'>
-                <div className='flex flex-col items-center justify-around ml-8'>
-                    <div className='text-2xl font-bold'>Total Available Funds Across Account</div>
-                    <div className=' text-6xl text-green-600 font-bold'>$amount</div>
-                    <div>{'(accounts + projected earnings - reserved)'}</div>
+            <div className="flex flex-row justify-between w-full">
+                <div className="flex flex-col items-center justify-around ml-8">
+                    <div className="text-2xl font-bold">Total Available Funds Across Account</div>
+                    <div className=" text-6xl text-green-600 font-bold">${ budgetsStore.totalFundsAvailable }</div>
+                    <div>{"(accounts + projected earnings - reserved)"}</div>
                 </div>
 
-                <div className='flex flex-col items-center'>
-                    <div className='text-2xl mt-4 font-bold'>Left to spend in May 2024</div>
+                <div className="flex flex-col items-center">
+                    <div className="text-2xl mt-4 font-bold">Left to spend in { currentMonth } { currentYear }</div>
                     <Gauge
                         width={400}
                         height={200}
-                        value={75}
-                        text='$2200'
+                        value={percentageRemaining}
+                        text={`$${remainingBudget}`}
                         startAngle={-90}
                         endAngle={90}
                         innerRadius="80%"
@@ -24,26 +43,26 @@ const SummaryComponent: React.FC = () => {
                         sx={{
                             [`& .${gaugeClasses.valueText}`]: {
                               fontSize: 40,
-                              transform: 'translate(0px, -20px)',
+                              transform: "translate(0px, -20px)",
                             },
                           }}
                         // ...
                     />
-                    <div className='bg-slate-200 p-1 px-2 rounded-lg font-bold'>of $3500</div>
+                    <div className="bg-slate-200 p-1 px-2 rounded-lg font-bold">of ${ budgetsStore.spendingBudget }</div>
                 </div>
                 
-                <div className='flex flex-col justify-around mr-8'>
-                    <div className='flex flex-col items-center'>
-                        <div className='text-2xl font-bold'>Total Spending Budget</div>
-                        <div className='text-lg'>$amount</div>
+                <div className="flex flex-col justify-around mr-8">
+                    <div className="flex flex-col items-center">
+                        <div className="text-2xl font-bold">Monthly Spending Budget</div>
+                        <div className="text-lg">${ budgetsStore.spendingBudget }</div>
                     </div>
-                    <div className='flex flex-col items-center'>
-                        <div className='text-2xl font-bold'>Allocated</div>
-                        <div className='text-lg'>$amount</div>
+                    <div className="flex flex-col items-center">
+                        <div className="text-2xl font-bold">Allocated</div>
+                        <div className="text-lg">${ budgetsStore.totalReserved }</div>
                     </div>
-                    <div className='flex flex-col items-center'>
-                        <div className='text-2xl font-bold'>Remaining</div>
-                        <div className='text-lg'>$amount</div>
+                    <div className="flex flex-col items-center">
+                        <div className="text-2xl font-bold">Remaining</div>
+                        <div className="text-lg">${ remainingBudget }</div>
                     </div>
                 </div>
             </div>
