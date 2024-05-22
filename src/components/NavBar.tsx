9fde5e7fa91b54b69305904ce5aa9f97d1975963
fridizkool <fsorgtaylor@gmail.com>
2024-05-22 13:41:01 -0400

    i18n button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,24 @@
 import { Link } from "react-router-dom";
-import { Icon, Modal, ModalToggleButton, ModalRef, ModalHeading, SideNav, Form, Fieldset, Label, TextInput, Button } from "@trussworks/react-uswds";
+import {
+    Icon,
+    Modal,
+    ModalToggleButton,
+    ModalRef,
+    ModalHeading,
+    SideNav,
+    Form,
+    Label,
+    TextInput,
+    Button,
+    LanguageSelector
+} from "@trussworks/react-uswds";
 import { useTranslation } from "react-i18next";
 import { useRef } from "react";
+import i18next from "i18next";
 
 const NavBar = () => {
     const { t } = useTranslation();
-    const modalRef = useRef<ModalRef>(null)
+    const modalRef = useRef<ModalRef>(null);
 
     const pages = [
         {
@@ -38,29 +51,24 @@ const NavBar = () => {
             title: t("tax.title"),
             icon: Icon.Assessment
         }
-    ]; 
-    const sideNavItems= [
+    ];
+    const sideNavItems = [
         <a href="#one" className="usa-current" key="one">
             Account
         </a>,
         <a href="#two" key="two">
             General
         </a>
-      ]
+    ];
 
     return (
-        <div className="nav-container bg-base-lighter px-6 h-screen min-w-64">
+        <div className="nav-container bg-base-lighter px-6 min-h-screen min-w-64 max-w-64">
             <div className="flex flex-col items-center">
                 <h1 className="font-semibold">{t("app-name")}</h1>
                 <div className="flex flex-row items-center gap-4">
                     <h3>{t("nav.greeting")}, [Name]</h3>
-                    <ModalToggleButton
-                        modalRef={modalRef}
-                        opener
-                        className="usa-button--unstyled"
-                        onClick={() => {}}
-                    >
-                        <Icon.Settings style={{fontSize: "1.4rem"}}/>
+                    <ModalToggleButton modalRef={modalRef} opener className="usa-button--unstyled" onClick={() => {}}>
+                        <Icon.Settings style={{ fontSize: "1.4rem" }} />
                     </ModalToggleButton>
                     <Modal
                         ref={modalRef}
@@ -72,56 +80,42 @@ const NavBar = () => {
                         <div className="flex flex-col justify-center bg-white w-full max-w-xl rounded-2xl">
                             <ModalHeading>Settings</ModalHeading>
                             <div className="flex mt-2">
-                                <SideNav items={sideNavItems}/>
+                                <SideNav items={sideNavItems} />
                                 <div className="flex w-full justify-center">
-                                    <Form onSubmit={()=> {}} className="w-9/12">
+                                    <Form onSubmit={() => {}} className="w-9/12">
                                         {/* <Fieldset legend={t("auth.login-desc")} legendStyle="default"> */}
-                                            <Label htmlFor="first-name">First Name</Label>
-                                            <TextInput
-                                                id="first-name"
-                                                name="first-name"
-                                                type="text"
-                                                autoComplete="first-name"
-                                            />
-                                            <Label htmlFor="last-name">Last Name</Label>
-                                            <TextInput
-                                                id="last-name"
-                                                name="last-name"
-                                                type="text"
-                                                autoComplete="last-name"
-                                            />
-                                            <Label htmlFor="email">{t("auth.email")}</Label>
-                                            <TextInput
-                                                id="email"
-                                                name="email"
-                                                type="text"
-                                                autoComplete="email"
-                                                disabled
-                                            />
+                                        <Label htmlFor="first-name">First Name</Label>
+                                        <TextInput
+                                            id="first-name"
+                                            name="first-name"
+                                            type="text"
+                                            autoComplete="first-name"
+                                        />
+                                        <Label htmlFor="last-name">Last Name</Label>
+                                        <TextInput
+                                            id="last-name"
+                                            name="last-name"
+                                            type="text"
+                                            autoComplete="last-name"
+                                        />
+                                        <Label htmlFor="email">{t("auth.email")}</Label>
+                                        <TextInput id="email" name="email" type="text" autoComplete="email" disabled />
 
-                                            <Label htmlFor="new-password">{t("auth.password")}</Label>
-                                            <TextInput
-                                                id="new-password"
-                                                name="new-password"
-                                                type={'text'}
-                                            />
-                                            <Label htmlFor="confirm-password">Confirm Password</Label>
-                                            <TextInput
-                                                id="confirm-password"
-                                                name="confirm-password"
-                                                type={'text'}
-                                            />
-                                            <button
-                                                type="button"
-                                                title="Toggle Password Visibility"
-                                                className="usa-show-password"
-                                                aria-controls="password"
-                                                onClick={() => {}}
-                                            >
-                                                {t("auth.show")}
-                                            </button>
+                                        <Label htmlFor="new-password">{t("auth.password")}</Label>
+                                        <TextInput id="new-password" name="new-password" type={"text"} />
+                                        <Label htmlFor="confirm-password">Confirm Password</Label>
+                                        <TextInput id="confirm-password" name="confirm-password" type={"text"} />
+                                        <Button
+                                            type="button"
+                                            title="Toggle Password Visibility"
+                                            className="usa-show-password"
+                                            aria-controls="password"
+                                            onClick={() => {}}
+                                        >
+                                            {t("auth.show")}
+                                        </Button>
 
-                                            <Button type="submit">Save</Button>
+                                        <Button type="submit">Save</Button>
                                         {/* </Fieldset> */}
                                     </Form>
                                 </div>
@@ -140,6 +134,24 @@ const NavBar = () => {
                         <pages.icon className="mr-3" /> {pages.title}
                     </Link>
                 ))}
+            </div>
+            <div className="flex flex-col items-center gap-6 absolute bottom-5">
+                <LanguageSelector
+                    className="w-full min-w-full text-left items-center"
+                    langs={[
+                        {
+                            attr: "es",
+                            label: "Cambiar al español",
+                            label_local: "Spanish - Mexican",
+                            on_click: () => i18next.changeLanguage("es")
+                        },
+                        {
+                            attr: "en",
+                            label: "Switch to English",
+                            on_click: () => i18next.changeLanguage("en")
+                        }
+                    ]}
+                />
             </div>
         </div>
     );
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,14 +2,14 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 const AppLayout: React.FC = () => {
-  return (
-    <div className="layout-container flex">
-      <NavBar />
-      <main className="main-content px-14 w-full h-screen overflow-y-scroll">
-        <Outlet />
-      </main>
-    </div>
-  );
+    return (
+        <div className="layout-container flex">
+            <NavBar />
+            <main className="main-content px-14 w-full h-screen overflow-y-scroll">
+                <Outlet />
+            </main>
+        </div>
+    );
 };
 
 export default AppLayout;
