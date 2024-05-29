33bfb5d8b4b6062120acf309547a746cd2a04624
Kaiwen Tsou <kktsou@ucsd.edu>
2024-05-28 17:20:23 -0700

    Revert "Updated Budgets Endpoint interactions to allow for connection through API gateway"
    This reverts commit b1201d087c5fea1f352154b755f6c655e0505ce9.

--- src/pages/Tax/taxesAPI.ts
+++ src/pages/Tax/taxesAPI.ts
@@ -14,17 +14,6 @@ interface fields {
     startingBalance: number
 };
 
-interface RawBudgetToSend {
-    //budgetId: number;
-    userId: number;
-    category: string;
-    totalAmount: number;
-    isReserved: boolean;
-    monthYear: string;
-    notes: string;
-    //createdTimestamp: string;
-};
-
 export const createTaxReturn = (initTaxReturn : initReturn) => {
     return apiClient.post(`/taxes/taxreturns`, initTaxReturn);
 }
@@ -44,32 +33,4 @@ export const getAccountByID = () => {
 export const postAccountData = (field : fields) => {
     console.log(field);
     return apiClient.post('/accounts/1', field);
-}
-
-export const getBudgetsMonthyear = (monthyear : string) => {
-    return apiClient.get(`/budgets/monthyear/${monthyear}/user/1`);
-}
-
-export const getTransactionsThing = (date : string, userid : number) => {
-    return apiClient.get(`/budgets/transactions/${date}/user/${userid}`);
-}
-
-export const getBudgetsTransactionsMonthyear = (monthyear : string) => {
-    return apiClient.get(`/budgets/transactions/${monthyear}/user/1`)
-}
-
-export const getBudgetsByIdAPI = () => {
-    return apiClient.get(`/budgets/1`);
-}
-
-export const createBudgetAPI = (budget: RawBudgetToSend) =>{
-    return apiClient.post(`/budgets`, budget);
-}
-
-export const deleteBudgetAPI = (id : number) => {
-    return apiClient.delete(`/budgets/${id}`)
-}
-
-export const updateBudgetAPI = (id : number, budget : RawBudgetToSend) => {
-    return apiClient.put(`/budgets/${id}`, budget);
 }
