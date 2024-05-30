96f3d8df68909044ace877cac16b257d8cd03db7
Koudou Sugawara <ksugawara01@gmail.com>
2024-05-30 09:01:48 -0400

    remove unused type - account

--- src/pages/Tax/taxesAPI.ts
+++ src/pages/Tax/taxesAPI.ts
@@ -1,9 +1,9 @@
 import apiClient from "./index";
-import { Transaction } from '../../types/models';
-import { W2State } from './W2Slice';
-import { taxReturn } from './TaxReturnSlice';
-import { otherIncome } from './otherIncomeSlice';
-import createApiClient from './index';
+import { Transaction } from "../../types/models";
+import { W2State } from "./W2Slice";
+import { taxReturn } from "./TaxReturnSlice";
+import { otherIncome } from "./otherIncomeSlice";
+import createApiClient from "./index";
 interface initReturn {
     year: number;
     userId: number;
@@ -43,27 +43,20 @@ type NewMonthlySummary = {
     totalBudgetAmount: number;
 };
 
-type Account = {
-    id: number;
+interface W2StateR {
+    state: string;
+    id?: number;
+    taxReturnId: number;
+    year: number;
     userId: number;
-    type: string;
-    currentBalance: number;
-};
-
-interface W2StateR{
-    "state": string,
-    "id"?: number,
-    "taxReturnId": number,
-    "year": number,
-    "userId": number,
-    "employer": string,
-    "wages": number,
-    "federalIncomeTaxWithheld": number,
-    "stateIncomeTaxWithheld": number,
-    "socialSecurityTaxWithheld": number,
-    "medicareTaxWithheld": number,
-    "imageKey": any
-};
+    employer: string;
+    wages: number;
+    federalIncomeTaxWithheld: number;
+    stateIncomeTaxWithheld: number;
+    socialSecurityTaxWithheld: number;
+    medicareTaxWithheld: number;
+    imageKey: any;
+}
 
 interface RawBucketToSend {
     // bucketId: number;
@@ -78,14 +71,12 @@ interface RawBucketToSend {
 }
 
 interface otherIncomeToSend {
-    
-        "taxReturnId": number,
-        "longTermCapitalGains": number,
-        "shortTermCapitalGains": number,
-        "otherInvestmentIncome": number,
-        "netBusinessIncome": number,
-        "additionalIncome": number
-    
+    taxReturnId: number;
+    longTermCapitalGains: number;
+    shortTermCapitalGains: number;
+    otherInvestmentIncome: number;
+    netBusinessIncome: number;
+    additionalIncome: number;
 }
 // export const initApiClient = () => {
 //     const apiClient = createApiClient(jwt);
@@ -99,9 +90,6 @@ interface ProfileType {
     id: number;
 }
 
-
-
-
 export const createTaxReturn = (initTaxReturn: initReturn) => {
     console.log("////////////////////////////////");
     console.log(initTaxReturn);
@@ -112,12 +100,11 @@ export const getTaxReturnById = (taxReturnId: number) => {
     return apiClient.get(`/taxes/taxreturns/${taxReturnId}`);
 };
 
-export const getTaxReturnByUserId = (jwt: string | null, userId : number) => {
-    
+export const getTaxReturnByUserId = (jwt: string | null, userId: number) => {
     console.log("////////////////////////////////");
     console.log(jwt);
     return apiClient.get(`/taxes/taxreturns`);
-}
+};
 
 export const getAccountByID = () => {
     return apiClient.get(`/accounts`);
@@ -128,10 +115,10 @@ export const postAccountData = (field: fields) => {
     return apiClient.post("/accounts", field);
 };
 
-export const getBudgetsMonthyear = ( monthyear: string) => {
+export const getBudgetsMonthyear = (monthyear: string) => {
     //const apiClient = createApiClient(jwt);
     return apiClient.get(`/budgets/monthyear/${monthyear}`);
-  };
+};
 
 export const getTransactionsThing = (date: string, userid: number) => {
     return apiClient.get(`/budgets/transactions/${date}/user/${userid}`);
@@ -167,43 +154,43 @@ export const deleteBudgetAPI = (id: number) => {
 
 export const updateBudgetAPI = (id: number, budget: RawBudgetToSend) => {
     return apiClient.put(`/budgets/${id}`, budget);
-}
+};
 
 export const getTransactionByUserIdAPI = (userId: number) => {
     return apiClient.get(`/transactions`);
-}
+};
 
-export const getAccountsByUserIdAPI = (userId:number) => {
+export const getAccountsByUserIdAPI = (userId: number) => {
     return apiClient.get(`/accounts`);
-}
+};
 
-export const deleteTransactionAPI = (transactionId:number) => {
+export const deleteTransactionAPI = (transactionId: number) => {
     return apiClient.delete(`/transactions/${transactionId}`);
-}
+};
 
 export const createTransactionAPI = (transaction: Omit<Transaction, "transactionId">) => {
     return apiClient.post(`/transactions`, transaction);
-}
+};
 
 export const updateTransactionAPI = (transactionId: number, transaction: Transaction) => {
     return apiClient.put(`/transactions/${transactionId}`, transaction);
-}
+};
 
-export const getTransactionByVendorAPI = (vendorName:string) => {
+export const getTransactionByVendorAPI = (vendorName: string) => {
     return apiClient.get(`/transactions/vendor/${vendorName}`);
-}
+};
 
 export const getRecentTransactionsAPI = () => {
     return apiClient.get(`/transactions/recentTransactions`);
-}
+};
 
 export const getCurrentMonthTransactionsAPI = () => {
     return apiClient.get(`/transactions/currentMonthTransactions`);
-}
+};
 
 export const deleteAccountAPI = (accountId: number) => {
     return apiClient.delete(`/accounts/${accountId}`);
-}
+};
 
 export const createW2API = (w2payload: Omit<W2State, "w2id">[]) => {
     const result: W2StateR[] = [];
@@ -225,66 +212,65 @@ export const createW2API = (w2payload: Omit<W2State, "w2id">[]) => {
     }
 
     return apiClient.post(`/taxes/w2s?taxReturnId=1`, result);
-}
+};
 
 export const createTaxReturnAPI = (taxPayload: Omit<taxReturn, "id">) => {
     return apiClient.put(`/taxes/taxreturns`, taxPayload);
-}
+};
 
 export const findW2sByTaxReturnIdAPI = () => {
     return apiClient.get(`/taxes/w2s?taxReturnId=1`);
-}
+};
 
 export const findAllDeductionsByTaxReturnAPI = () => {
     return apiClient.get(`/taxes/taxreturns/1/deductions`);
-}
+};
 
 export const getBucketsAPI = () => {
     return apiClient.get(`/buckets/user/1`);
-}
+};
 
-export const addBucketsAPI = (bucket : RawBucketToSend) => {
+export const addBucketsAPI = (bucket: RawBucketToSend) => {
     return apiClient.post(`/buckets/add`, bucket);
-}
+};
 
-export const updateBucketAPI = (bucket : RawBucketToSend, id:number) => {
+export const updateBucketAPI = (bucket: RawBucketToSend, id: number) => {
     return apiClient.put(`/buckets/update/${id}`, bucket);
-}
+};
 
 export const deleteBucketAPI = (id: number) => {
     return apiClient.delete(`/buckets/delete/${id}`);
-}
+};
 
 export const updateTaxReturnAPI = (payload: Partial<taxReturn>, id: number | undefined) => {
     return apiClient.put(`/taxes/taxreturns/${id}`, payload);
-}
+};
 
-export const addOtherIncomeAPI = (payload : otherIncome) => {
-    const actual_payload : otherIncomeToSend = {
-        taxReturnId : payload.oitaxReturnId,
-        longTermCapitalGains : payload.oilongTermCapitalGains,
-        shortTermCapitalGains : payload.oishortTermCapitalGains,
-        otherInvestmentIncome : payload.oiotherInvestmentIncome,
-        netBusinessIncome : payload.oinetBusinessIncome,
-        additionalIncome : payload.oiadditionalIncome
-    }
+export const addOtherIncomeAPI = (payload: otherIncome) => {
+    const actual_payload: otherIncomeToSend = {
+        taxReturnId: payload.oitaxReturnId,
+        longTermCapitalGains: payload.oilongTermCapitalGains,
+        shortTermCapitalGains: payload.oishortTermCapitalGains,
+        otherInvestmentIncome: payload.oiotherInvestmentIncome,
+        netBusinessIncome: payload.oinetBusinessIncome,
+        additionalIncome: payload.oiadditionalIncome
+    };
     return apiClient.post(`/taxes/other-income`, actual_payload);
-}
+};
 
 export const getOtherIncomeAPI = () => {
     return apiClient.get(`/taxes/other-income/1`);
-}
+};
 
 export const deleteTaxReturn = (id: number | undefined) => {
     return apiClient.delete(`/taxes/taxreturns/${id}`);
-}
+};
 
 export const getUserInformationAPI = () => {
     return apiClient.get(`/users/user`);
-}
+};
 
-export const updateUserInfo = (profile : ProfileType) => {
-    console.log("UPDATE PROFILE: ", profile)
+export const updateUserInfo = (profile: ProfileType) => {
+    console.log("UPDATE PROFILE: ", profile);
     return apiClient.put(`/users`, profile);
-}
-
+};
