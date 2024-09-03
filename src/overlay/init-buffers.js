aed2289c6b3b04c6f02f2c455c49ed96c5c3572a
Kaiwen Tsou <kktsou@ucsd.edu>
2024-09-03 09:04:07 -0700

    Additional starter code

--- src/overlay/init-buffers.js
+++ src/overlay/init-buffers.js
@@ -1,8 +1,10 @@
 function initBuffers(gl) {
     const positionBuffer = initPositionBuffer(gl);
+    const colorBuffer = initColorBuffer(gl);
 
     return {
-        position: positionBuffer
+        position: positionBuffer,
+        color: colorBuffer
     };
 }
 
@@ -15,7 +17,8 @@ function initPositionBuffer(gl) {
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
     // Now create an array of positions for the square.
-    const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
+    // TODO- This is currently hard coded. needs to be applied screenwide.
+    const positions = [5, 5, -5, 5, 5, -5, -5, -5];
 
     // Now pass the list of positions into WebGL to build the
     // shape. We do this by creating a Float32Array from the
@@ -23,6 +26,33 @@ function initPositionBuffer(gl) {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
     return positionBuffer;
+}
+
+function initColorBuffer(gl) {
+    const colors = [
+        1.0,
+        1.0,
+        1.0,
+        1.0, // white
+        1.0,
+        0.0,
+        0.0,
+        1.0, // red
+        0.0,
+        1.0,
+        0.0,
+        1.0, // green
+        0.0,
+        0.0,
+        1.0,
+        1.0 // blue
+    ];
+
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    return colorBuffer;
 }
 
 export { initBuffers };
--- src/overlay/webgl.js
+++ src/overlay/webgl.js
@@ -1,6 +1,9 @@
 import { initBuffers } from "./init-buffers.js";
 import { drawScene } from "./draw-scene.js";
 
+let squareRotation = 0.0;
+let deltaTime = 0;
+
 export function webGLMain(canvas) {
     // const canvas = document.querySelector("#glcanvas");
     // Initialize the GL context
@@ -13,34 +16,48 @@ export function webGLMain(canvas) {
     }
 
     // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 0.0, 0.5);
+    gl.clearColor(0.0, 0.0, 0.0, 0.0);
     // Clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     // Vertex shader program
+
     const vsSource = `
         attribute vec4 aVertexPosition;
+        attribute vec4 aVertexColor;
+
         uniform mat4 uModelViewMatrix;
         uniform mat4 uProjectionMatrix;
-        void main() {
+
+        varying lowp vec4 vColor;
+
+        void main(void) {
         gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+        vColor = aVertexColor;
         }
-        `;
+    `;
+
+    // Fragment shader program
+
     const fsSource = `
-        void main() {
-        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        varying lowp vec4 vColor;
+
+        void main(void) {
+        gl_FragColor = vColor;
         }
     `;
 
     const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
 
     // Collect all the info needed to use the shader program.
-    // Look up which attribute our shader program is using
-    // for aVertexPosition and look up uniform locations.
+    // Look up which attributes our shader program is using
+    // for aVertexPosition, aVertexColor and also
+    // look up uniform locations.
     const programInfo = {
         program: shaderProgram,
         attribLocations: {
-            vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition")
+            vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+            vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor")
         },
         uniformLocations: {
             projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
@@ -52,7 +69,22 @@ export function webGLMain(canvas) {
     // objects we'll be drawing.
     const buffers = initBuffers(gl);
     // Draw the scene
-    drawScene(gl, programInfo, buffers);
+    // drawScene(gl, programInfo, buffers);
+
+    let then = 0;
+
+    // Draw the scene repeatedly
+    function render(now) {
+        now *= 0.001; // convert to seconds
+        deltaTime = now - then;
+        then = now;
+
+        drawScene(gl, programInfo, buffers, squareRotation);
+        squareRotation += deltaTime;
+
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
 }
 
 //
