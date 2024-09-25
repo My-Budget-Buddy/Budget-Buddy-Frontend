1035b8d59fe43b4468cac05002bbda16e51ae607
Bryar-Frank <54441650+Bryar-Frank@users.noreply.github.com>
2024-09-25 13:10:38 -0400

    missing some mocks, added now

--- src/__tests__/landing.test.tsx
+++ src/__tests__/landing.test.tsx
@@ -4,6 +4,20 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from 'react-router-dom';
 import '@testing-library/jest-dom';
 
+
+jest.mock("../api/config", () => ({
+    config: {
+      apiUrl: "http://localhost:mock",
+    },
+}));
+
+//Image mocks
+jest.mock('../../assets/budgets/Mar-Business_11.jpg', () => 'budgetImg');
+jest.mock('../../assets/taxes/Wavy_Tech-03_Single-12.jpg', () => 'taxImg');
+jest.mock('../../assets/transactions/20943914.jpg', () => 'transactionsImg');
+jest.mock('../../assets/hero.jpg', () => 'heroBg');
+
+// Style mocks
 jest.mock('./LandingPage.css', () => ({}));
 
 beforeEach(() => {
