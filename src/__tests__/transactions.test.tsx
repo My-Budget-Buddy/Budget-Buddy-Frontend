41a44804835be199833ab8e63f8793fb54bce9b4
jmensah365 <jmensah365@gmail.com>
2024-09-25 10:10:48 -0400

    working on jest testing

--- src/__tests__/transactions.test.tsx
+++ src/__tests__/transactions.test.tsx
@@ -1,77 +1,30 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import Transactions from "../pages/Transactions/Transactions";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from 'react-router-dom';
-import Transactions from '../pages/Transactions/Transactions';
-import { validateTransaction, getTransactionByUserId, deleteTransaction, getAccountsByUserId, createTransaction, updateTransaction, getTransactionByVendor } from '../utils/transactionService';
-import { createTransactionAPI, deleteTransactionAPI, getAccountsByUserIdAPI, getTransactionByUserIdAPI, getTransactionByVendorAPI, updateTransactionAPI } from "../pages/Tax/taxesAPI"; 
 import '@testing-library/jest-dom';
-import { Account, Transaction, TransactionCategory } from '../types/models';
-
-// Mock the API calls
-jest.mock('../pages/Tax/taxesAPI');
 
 jest.mock('../utils/transactionService');
+jest.mock('../pages/Tax/taxesAPI');
 
-// Mock the useTranslation hook
+//Mock the useTranslation hook
 jest.mock('react-i18next', () => ({
     useTranslation: () => ({
-      t: (key: string) => key,
-      i18n: {
-        changeLanguage: jest.fn(),
-        language: 'en',
-      },
+        t: (key: string) => key,
     }),
-  }));
+}));
 
-describe('Transactions component', () => {
-    const mockTransaction: Transaction = {
-        transactionId: 1,
-        userId: 1,
-        vendorName: 'Vendor',
-        amount: 100,
-        date: '2022-01-01',
-        category: "Category" as TransactionCategory,
-        accountId: 1,
-        description: 'Description',
-    };
+beforeEach(() => {
+    render(
+        <Router>
+            <Transactions />
+        </Router>
+    );
+});
 
-    const mockAccount: Account = {
-        id: 1,
-        userId: '123',
-        accountNumber: "Test Account",
-        type: "CHECKING",
-        routingNumber: "123456789",
-        institution: "Test Bank",
-        investmentRate: 0.01,
-        startingBalance: 1000,
-        currentBalance: 1000,
-    };
-    beforeEach(() => {
-    // Mock the transactionService functions
-    (getTransactionByUserIdAPI as jest.Mock).mockResolvedValue({ data: [mockTransaction] });
-    (deleteTransaction as jest.Mock).mockResolvedValue({data: null});
-    (getAccountsByUserId as jest.Mock).mockResolvedValue({ data: [mockAccount] });
-    (createTransaction as jest.Mock).mockResolvedValue({ data: [mockTransaction] });
-    (updateTransaction as jest.Mock).mockResolvedValue({data: [mockTransaction]});
-    (getTransactionByVendor as jest.Mock).mockResolvedValue({ data: [mockTransaction] });
-    (validateTransaction as jest.Mock).mockReturnValue({data: []});
-        render(
-            <Router>
-                <Transactions />
-            </Router>
-        );
-    });
+afterEach(() => {
+    jest.clearAllMocks();
+});
 
-    // test('renders without crashing', () => {
-    //     const transactionsComponent = screen.getByTestId('transactions');
-    //     expect(transactionsComponent).toBeInTheDocument();
-    // });
-
-    test('renders transaction title', () => {
-        expect(screen.getByText('transactions.list-of-transactions')).toBeInTheDocument();
-    });
-
-    // test('renders transaction table', () => {
-    //     expect(screen.getByTestId('transaction-table')).toBeInTheDocument();
-    // });
+it('renders without crashing', () => {
+    expect(screen.getByText('transactions.title')).toBeInTheDocument();
 });
