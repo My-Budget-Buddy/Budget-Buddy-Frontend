e519fd1b2191c7223a25c0d744875795d60145ab
Aaron Huggins <achuggi1@asu.edu>
2024-09-26 21:45:05 -0700

    Finalize sonarqube

--- sonarqube-scanner.cjs
+++ sonarqube-scanner.cjs
@@ -3,19 +3,21 @@ const env = require('node:process').env;
 
 scanner(
     {
-      serverUrl: 'https://sonarcloud.io/', // env.SONAR_SERVER_URL,
-      token: '7631d4ee8f875e5df09ae9e98110e9022590c055', // env.SONAR_TOKEN,
+      serverUrl: env.SONAR_SERVER_URL,
+      token: env.SONAR_TOKEN,
       options: {
         'sonar.organization': 'my-budget-buddy',
 
-        'sonar.projectName': 'Budget-Buddy-Frontend', // env.SONAR_PROJECT_NAME,
-        'sonar.projectKey': 'My-Budget-Buddy_Budget-Buddy-Frontend', // env.SONAR_PROJECT_KEY,
-        'sonar.projectDescription': 'SonarQube project for ' + 'Budget-Buddy-Frontend',// env.SONAR_PROJECT_NAME,
+        'sonar.projectName': env.SONAR_PROJECT_NAME,
+        'sonar.projectKey':  env.SONAR_PROJECT_KEY,
+        'sonar.projectDescription': 'SonarQube project for ' + env.SONAR_PROJECT_NAME,
 
-        'sonar.branch.name': 'testing-cohort-dev', // env.BRANCH_NAME,
+        'sonar.branch.name': env.BRANCH_NAME,
 
         'sonar.sources': 'src',
-        'sonar.tests': 'src/__tests__',
+        'sonar.exclusions': 'src/App.tsx,src/i18n.ts,src/main.tsx,src/vite-env.d.ts,src/api/**,src/api/config.ts,src/components/**,src/contexts/**,src/layouts/**,src/pages/Root.tsx,src/pages/Budgets/components/modals/**,src/pages/Budgets/components/requests/**,src/pages/Budgets/components/subComponents/**,src/pages/Budgets/components/util/**,src/pages/Tax/TaxesAPI.ts,src/routing/**,src/util/**',
+        'sonar.tests': '__tests__',
+        'sonar.testExecutionReportPaths': 'test-report.xml',
         'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info'
       },
     },
